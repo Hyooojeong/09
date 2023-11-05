61eef90fb46d0f08a5a834b1cf576ffb065dfd55
Hyooojeong <hjj001028@naver.com>
2023-11-05 23:16:06 +0900

    5

--- main.c
+++ main.c
@@ -1,33 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SIZE           4
+#define ROWS        3
+#define COLS        3
 
-void square_array (int a[], int size)
-{
-	int i;
-	
-	for (i=0; i<size; i++)
-	a[i] = a[i] * a[i];
-}
 
-void print_array(int a[], int size)
+void addMatrix(int A[ROWS] [COLS], int B[ROWS] [COLS], int C[ROWS] [COLS])
 {
-	int i;
-	
-	for (i=0; i<size; i++)
-	  printf("%3d", a[i]);
-	print("\n");
+	//C = A + B : for loop
+	for (int i = 0; i < ROWS; i++) {
+		for (int j = 0; j < COLS; j++) {
+			C[i][j] = A[i][j] + B[i][j];
+		}
+	}
+} 
+
+void printMatrix(int A[ROWS] [COLS])
+{
+	int i, j;
+	//for loop - A의 요소를 각자 출력 
+	for (int i = 0; i < ROWS; i++) {
+		for (int j = 0; j < COLS; j++) {
+			print("%d ", A[i][j]);
+		}
+		printf("\n");
+	}
 }
 
 int main(int argc, char *argv[]) 
 {
-	int list[SIZE] = {1, 2, 3, 4};
+	int A[ROWS] [COLS] = {
+	    {2, 3, 0},
+		{8, 9, 1},
+		{7, 0, 5}};
+	int B[ROWS] [COLS] = {
+	    {1, 0, 0},
+	    {0, 1, 0},
+		{0, 0, 1}};
+	int C[ROWS] [COLS];
 	
-	print_array(list, SIZE);
-	squre_array(list, SIZE);
-	print_array(list, SIZE);
-	  
-	system("PAUSE");
-  	return 0;
+	addMatrix(A,B,C);
+	printMatrix(C);
+	
+	syetem("PAUSE");
+	return 0;
 }
